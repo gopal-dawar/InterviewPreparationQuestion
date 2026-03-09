26917925d761e73f000e532bb69498c485691240
gopal-dawar <gopaldawar45@gmail.com>
2026-03-09 15:00:19 +0530

    reverse string

---
+++ src/assing1/Qustion7.java
@@ -0,0 +1,16 @@
+package assing1;
+
+import java.util.Scanner;
+
+public class Qustion7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name : ");
+		String name = sc.nextLine();
+
+		for (int i = name.length() - 1; i >= 0; i--) {
+			System.out.print(name.charAt(i));
+		}
+
+	}
+}
