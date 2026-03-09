c6a639a8334bcb21a9b23bcd545acb5fabbf4876
gopal-dawar <gopaldawar45@gmail.com>
2026-03-09 16:07:41 +0530

    sort array without using sort predefined methods

---
+++ src/assing1/Question14.java
@@ -0,0 +1,20 @@
+package assing1;
+
+public class Question14 {
+	public static void main(String[] args) {
+		int[] arr = { 8, 7, 2, 3, 1 };
+
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[j] < arr[i]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
