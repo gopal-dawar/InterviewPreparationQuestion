d2d77d23ceefac3e6869391ec74ede0262362808
gopal-dawar <gopaldawar45@gmail.com>
2026-03-09 14:30:06 +0530

    Fibonacci series

---
+++ src/assing1/Question3.java
@@ -0,0 +1,22 @@
+package assing1;
+
+import java.util.Scanner;
+
+public class Question3 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int n = sc.nextInt();
+
+		int a = 0;
+		int b = 1;
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(a + " ");
+			int next = a + b;
+			a = b;
+			b = next;
+		}
+	}
+}
