70c60e4d509e179997498af7bd6e2b900fb71068
gopal-dawar <gopaldawar45@gmail.com>
2026-03-10 22:33:41 +0530

    new question

--- src/assign2/Question17.java
+++ src/assign2/Question17.java
@@ -2,26 +2,26 @@ package assign2;
 
 public class Question17 {
 	public static void main(String[] args) {
-		int[] arr1 = { 8, 7, 3, 1, 2 };
-		int[] arr2 = { 4, 5, 6, 7, 8, 9, 11, 2, 3, 44 };
-		int[] arr3 = new int[arr1.length + arr2.length + 1];
-
-		int count = 0;
-
-		for (int i = 0; i < arr1.length; i++) {
-
-			arr3[count++] = arr1[i];
-		}
-
-		for (int i = 0; i < arr3.length; i++) {
-			if (arr3[i] != arr2[i]) {
-				arr3[arr1.length + i] = arr2[i];
-				count++;
-			}
-		}
-
-		for (int i = 0; i < count; i++) {
-			System.out.print(arr3[i] + " ");
-		}
+//		int[] arr1 = { 8, 7, 3, 1, 2 };
+//		int[] arr2 = { 4, 5, 6, 7, 8, 9, 11, 2, 3, 44 };
+//		int[] arr3 = new int[arr1.length + arr2.length + 1];
+//
+//		int count = 0;
+//
+//		for (int i = 0; i < arr1.length; i++) {
+//
+//			arr3[count++] = arr1[i];
+//		}
+//
+//		for (int i = 0; i < arr3.length; i++) {
+//			if (arr3[i] != arr2[i]) {
+//				arr3[arr1.length + i] = arr2[i];
+//				count++;
+//			}
+//		}
+//
+//		for (int i = 0; i < count; i++) {
+//			System.out.print(arr3[i] + " ");
+//		}
 	}
 }
